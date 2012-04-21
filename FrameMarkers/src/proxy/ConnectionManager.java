05acafb44f11f22f553524511e0f745947670713
uri@salsa4fun.co.il <uri@salsa4fun.co.il@47add45c-000b-5f58-625e-14702521dcaa>
2012-04-21 19:24:56 +0000

    Fixed server's port + parameter names + remove dead code

--- FrameMarkers/src/proxy/ConnectionManager.java
+++ FrameMarkers/src/proxy/ConnectionManager.java
@@ -1,23 +1,15 @@
 package proxy;
 
 import java.io.IOException;
-import java.io.OutputStream;
 import java.net.URLConnection;
-import java.util.Arrays;
 
-import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.conn.params.ConnRoutePNames;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import com.qualcomm.QCARSamples.FrameMarkers.DebugLog;
-
-import android.util.Log;
 
 public class ConnectionManager {
 	String url = "http://www.google.com/";
@@ -41,13 +33,13 @@ public class ConnectionManager {
 			HttpClient client = new DefaultHttpClient();
 			String cameraIdString = Integer.toString(cameraId);
 			HttpPost post = new HttpPost(
-					"http://192.168.0.100:8080/api/cameras/" + cameraIdString);
+					"http://192.168.0.100:1280/api/cameras/" + cameraIdString);
 			DebugLog.LOGD("created post!");
 			post.setHeader("Content-type", "application/x-www-form-urlencoded");
 			post.setHeader("Accept", "*/*");
 
 			String message = String
-					.format("marker=%d&x=%.4f&y=%.4f&z=%.4f&poseMatrix=%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f",
+					.format("marker=%d&x=%.4f&y=%.4f&z=%.4f&matrix=%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f",
 							markerId, x, y, z, poseMatrix0, poseMatrix1,
 							poseMatrix2, poseMatrix3, poseMatrix4, poseMatrix5,
 							poseMatrix6, poseMatrix7, poseMatrix8, poseMatrix9,
@@ -64,20 +56,6 @@ public class ConnectionManager {
 			DebugLog.LOGD("ERROR! " + e.getMessage());
 		}
 
-	}
-
-	void writeFloat(OutputStream out, float aFloat) throws IOException {
-		int floatBits = Float.floatToIntBits(aFloat);
-
-		writeInt(out, floatBits);
-	}
-
-	void writeInt(OutputStream out, int anInt) throws IOException {
-		out.write(anInt & 0x000000FF);
-		out.write((anInt & 0x0000FF00) >> 8);
-		out.write((anInt & 0x00FF0000) >> 16);
-		out.write((anInt & 0xFF000000) >> 24);
-		out.flush();
 	}
 
 	public native void register();
